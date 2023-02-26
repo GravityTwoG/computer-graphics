bf039772a3a12321f460993277caea8c99e15137
GravityTwoG <crytekov@gmail.com>
2023-02-26 18:41:46 +0700

    build: include fill.html to build

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// vite.config.js
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [],
+  build: {
+    rollupOptions: {
+      input: {
+        index: fileURLToPath(new URL('./index.html', import.meta.url)),
+        fill: fileURLToPath(new URL('./pages/fill.html', import.meta.url)),
+      },
+    },
+  },
+});
